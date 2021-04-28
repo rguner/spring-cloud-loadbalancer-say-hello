7fc82fce89230f7701eefc825a0118a4b7808455
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2021-04-28 19:34:31 +0300

    reactor denemeler

--- src/main/java/com/example/springcloudloadbalancersayhello/SpringCloudLoadbalancerSayHelloApplication.java
+++ src/main/java/com/example/springcloudloadbalancersayhello/SpringCloudLoadbalancerSayHelloApplication.java
@@ -2,6 +2,7 @@ package com.example.springcloudloadbalancersayhello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,8 @@ import java.util.Random;
 public class SpringCloudLoadbalancerSayHelloApplication {
 
 
+    @Value("${server.port}")
+    private int serverPort;
 
     private static Logger log = LoggerFactory.getLogger(SpringCloudLoadbalancerSayHelloApplication.class);
 
@@ -32,13 +35,13 @@ public class SpringCloudLoadbalancerSayHelloApplication {
         Random rand = new Random();
 
         int randomNum = rand.nextInt(greetings.size());
-        return greetings.get(randomNum);
+        return greetings.get(randomNum) + " " + serverPort;
     }
 
     @GetMapping("/")
     public String home() {
         log.info("Access /");
-        return "Hi!";
+        return "Hi " + serverPort;
     }
 
 }
